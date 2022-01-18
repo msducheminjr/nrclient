2dc5612290a2e77edd01077fa8f205e571bc4afc
Michael Duchemin <msducheminjr@users.noreply.github.com>
2022-01-18 16:14:57 -0500

    Clear form and add debounce
    Clear the search form and hide the results if the user clicks outside of
    the form or the search results.
    
    Also add a debounce function that starts a timeout and clears the
    timeout if new input is reached before the timeout is reached. If new
    input is received the old timer gets cleared and the new function
    replaces it with the timer reset.

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,9 +2,41 @@ import { Controller } from "@hotwired/stimulus"
 import "form-request-submit-polyfill"
 
 export default class extends Controller {
-  static targets = ["form", "input"]
+  static targets = ["form", "input", "results"]
+
+  basicSubmit() {
+    if (this.inputTarget.value === "") {
+      this.reset()
+    } else {
+      this.formTarget.requestSubmit()
+    }
+  }
 
   submit() {
-    this.formTarget.requestSubmit()
+    this.debounce(this.basicSubmit.bind(this))()
+  }
+
+  debounce(functionToDebounce, wait = 300) {
+    let timeoutId = null
+
+    return (...args) => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+        timeoutId = null
+        functionToDebounce(...args)
+      }, wait)
+    }
+  }
+
+  resetOnOutsideClick(event) {
+    if (!this.element.contains(event.target)) {
+      this.reset()
+    }
+  }
+
+  reset() {
+    this.resultsTarget.classList.add("hidden")
+    this.resultsTarget.innerText = ""
+    this.inputTarget.value = ""
   }
 }
